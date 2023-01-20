33e9b7477515ee6d9e9ef4fbf15429830c830cb3
Kgavaga <jaspergavagan@gmail.com>
2023-01-20 20:21:00 +0100

    Removed moving in the world, Added Mouse Drag functionality, Updated the Shader, Removed old file

--- index.js
+++ index.js
@@ -3,67 +3,20 @@
 
 var mousePosX = 0;
 var mousePosY = 0;
+var mousedown = false;
 
 function initMouseInput(){
   onmousemove = function(e){
     mousePosX = e.clientX;
     mousePosY = e.clientY;
   }
-}
 
-const NOT_PRESSED = 0.0;
-const PRESSED = 1.0;
-
-var forwardKeyPressed = NOT_PRESSED;
-var backwardKeyPressed = NOT_PRESSED;
-var leftKeyPressed = NOT_PRESSED;
-var rightKeyPressed = NOT_PRESSED;
-var spaceKeyPressed = NOT_PRESSED;
-var shiftKeyPressed = NOT_PRESSED;
-
-function initKeyboardInput(){
-  onkeydown = function(e) {
-      console.log(e.key);
-      if(e.key == "w") {
-        forwardKeyPressed = PRESSED;
-      }
-      else if(e.key == "s") {
-        backwardKeyPressed = PRESSED;
-      }
-      else if(e.key == "a") {
-        leftKeyPressed = PRESSED;
-      }
-      else if(e.key == "d") {
-        rightKeyPressed = PRESSED;
-      }
-      else if(e.key == " ") {
-        spaceKeyPressed = PRESSED;
-      }
-      else if(e.key == "Shift") {
-        shiftKeyPressed = PRESSED;
-      }
+  onmousedown = function(e){
+    mousedown = true;
   }
 
-  onkeyup = function(e) {
-    console.log(e.key);
-    if(e.key == "w") {
-      forwardKeyPressed = NOT_PRESSED;
-    }
-    else if(e.key == "s") {
-      backwardKeyPressed = NOT_PRESSED;
-    }
-    else if(e.key == "a") {
-      leftKeyPressed = NOT_PRESSED;
-    }
-    else if(e.key == "d") {
-      rightKeyPressed = NOT_PRESSED;
-    }
-    else if(e.key == " ") {
-      spaceKeyPressed = NOT_PRESSED;
-    }
-    else if(e.key == "Shift") {
-      shiftKeyPressed = NOT_PRESSED;
-    }
+  onmouseup = function(e){
+    mousedown = false;
   }
 }
 
@@ -125,9 +78,7 @@ function setRectangle(gl, x, y, width, height) {
 }                
 
 function main() {
-
   initMouseInput();
-  initKeyboardInput();
   var gl = initOpenGl();
   var program = compileProgram(gl);
 
@@ -139,9 +90,6 @@ function main() {
   var fragmentResolutionUniformLocation = gl.getUniformLocation(program, "iResolution");
   var mouseUniformLocation = gl.getUniformLocation(program, "iMouse");
   var timeUniformLocation = gl.getUniformLocation(program, "iTime");
-  var cameraPositionUniformLocation = gl.getUniformLocation(program, "cameraPosition");
-  var cameraOrientationUniformLocation = gl.getUniformLocation(program, "cameraOrientation");
-
 
   // Create a vertex array object (attribute state)
   var vao = gl.createVertexArray();
@@ -182,38 +130,17 @@ function main() {
   // Set all constant uniform values
   gl.uniform2f(vertexResolutionUniformLocation, gl.canvas.width, gl.canvas.height);
   gl.uniform2f(fragmentResolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-  
-  var currentPosition = [0.0, 0.0, 0.0]
-  var currentOrientation = [0.0, 0.0, 0.0]
-  var lastTime = 0;
+
   requestAnimationFrame(drawScene);
 
   function drawScene(timeInMilliseconds){
     var timeInSeconds = timeInMilliseconds/1000;
-    // Subtract the previous time from the current time
-    var deltaTime = timeInSeconds - lastTime;
-    // Remember the current time for the next frame.
-    lastTime = timeInSeconds;
 
-    gl.uniform2f(mouseUniformLocation, mousePosX, mousePosY);
+    // Update position of mouse and time
+    if(mousedown){
+      gl.uniform2f(mouseUniformLocation, mousePosX, mousePosY);
+    }
     gl.uniform1f(timeUniformLocation, timeInSeconds);
-    
-    var mouseSpeed = 0.01;
-    var mouseSpeedBasedOnFps = deltaTime * mouseSpeed;
-    currentOrientation[0] = mousePosX * mouseSpeed;
-    currentOrientation[1] = mousePosY * mouseSpeed;
-
-    var movementSpeed = 3.0;spaceKeyPressed
-    var movementSpeedBasedOnFps = movementSpeed*deltaTime;
-    currentPosition[0] -= leftKeyPressed * movementSpeedBasedOnFps;
-    currentPosition[0] += rightKeyPressed * movementSpeedBasedOnFps;
-    currentPosition[1] -= shiftKeyPressed * movementSpeedBasedOnFps;
-    currentPosition[1] += spaceKeyPressed * movementSpeedBasedOnFps;
-    currentPosition[2] -= forwardKeyPressed * movementSpeedBasedOnFps;
-    currentPosition[2] += backwardKeyPressed * movementSpeedBasedOnFps;
-
-    gl.uniform3f(cameraPositionUniformLocation, currentPosition[0], currentPosition[1], currentPosition[2]);
-    gl.uniform3f(cameraOrientationUniformLocation, currentOrientation[0], currentOrientation[1], currentOrientation[2]);
 
     // Render the scene
     render(gl); 
